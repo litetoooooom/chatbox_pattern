777b122b61e629e0a706f7bb229496a3013ec63d
Tom <zoujinyong@putao.com>
2019-03-26 10:20:16 +0800

    update

--- trie.py
+++ trie.py
@@ -1,85 +1,122 @@
-# -*- coding: utf-8 -*-
-from typing import Tuple
+from collections import defaultdict
+from collections.abc import Set
 
+class TrieNode(Set):
+    def __init__(self, iterable=()):
+        self._children = defaultdict(TrieNode)
+        self._end = False
+        self._len = 0
+        for element in iterable:
+            self.add(element)
 
-class TrieNode(object):
-    """
-    Our trie node implementation. Very basic. but does the job
-    """
+    def add(self, element):
+        node = self
+        for s in element:
+            node = node._children[s]
+        if not node._end:
+            node._end = True
+            node._len += 1
+            node = self
+            for s in element:
+                node._len += 1
+                node = node._children[s]
+
+    def __contains__(self, element):
+        node = self
+        for k in element:
+            if k not in node._children:
+                return False
+            node = node._children[k]
+        return node._end
     
-    def __init__(self, char: str):
-        self.char = char
-        self.children = []
-        # Is it the last character of the word.`
-        self.word_finished = False
-        # How many times this character appeared in the addition process
-        self.counter = 1
+    def search_1(self, term):
+        results = set()
+        element = []
+        def _search(m, node, i):
+            element.append(m)
+            if i == len(term):
+                if node._end:
+                    results.add(''.join(element))
+            elif term[i] == '?':
+                for k,child in node._children.items():
+                    _search(k, child, i+1)
+            elif term[i] == '*':
+                _search('', node, i+1)
+                for k,child in node._children.items():
+                    _search(k, child, i)
+            elif term[i] in node._children:
+                _search(term[i], node._children[term[i]], i+1)
+            element.pop()
+        _search('', self, 0)
+
+        return results
     
+    def search_2(self, term):
+        def _add(indexes, i):
+            while i < len(term) and term[i] == '*':
+                indexes.add(i)
+                i += 1
+            indexes.add(i)
+        
+        indexes = set()
+        _add(indexes, 0)
+        if self._end and len(term) in indexes:
+            yield ''
+        
+        indexes_stack = [indexes]
+        element = ['']
+        iter_stack = [iter(self._children.items())]
+        while iter_stack:
+            for k, node in iter_stack[-1]:
+                new_indexes = set()
+                for i in indexes_stack[-1]:
+                    if i >= len(term):
+                        continue
+                    elif term[i] == '*':
+                        _add(new_indexes, i)
+                    elif term[i] == '?' or term[i] == k:
+                        _add(new_indexes, i + 1)
+                if new_indexes:
+                    element.append(k)
+                    if node._end and len(term) in new_indexes:
+                        yield ''.join(element)
+                    indexes_stack.append(new_indexes)
+                    iter_stack.append(iter(node._children.items()))
+                    break
+            else:
+                element.pop()
+                indexes_stack.pop()
+                iter_stack.pop()
 
-def add(root, word: str):
-    """
-    Adding a word in the trie structure
-    """
-    node = root
-    for char in word:
-        found_in_child = False
-        # Search for the character in the children of the present `node`
-        for child in node.children:
-            if child.char == char:
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-                child.counter += 1
-                # And point the node to the child that contains this char
-                node = child
-                found_in_child = True
+    def __iter__(self):
+        element = ['']
+        stack = [iter([('', self)])]
+        while stack:
+            for k,node in stack[-1]:
+                element.append(k)
+                if node._end:
+                    yield ''.join(element)
+                stack.append(iter(node._children.items()))
                 break
-        # We did not find it so add a new chlid
-        if not found_in_child:
-            new_node = TrieNode(char)
-            node.children.append(new_node)
-            # And then point node to the new child
-            node = new_node
-    # Everything finished. Mark it as the end of a word.
-    node.word_finished = True
+            else:
+                element.pop()
+                stack.pop()
+
+    def __len__(self):
+        return self._len
 
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
-    """
-    Check and return 
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
-    """
-    node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
-    if not root.children:
-        return False, 0
-    for char in prefix:
-        char_not_found = True
-        # Search through all the children of the present `node`
-        for child in node.children:
-            if child.char == char:
-                # We found the char existing in the child.
-                char_not_found = False
-                # Assign node as the child containing the char and break
-                node = child
-                break
-        # Return False anyway when we did not find a char.
-        if char_not_found:
-            return False, 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
-    return True, node.counter
+root = TrieNode()
+root.add("aba*")
+root.add("abb")
+root.add("abc")
+root.add("abd")
+print(root.search_1("*b"))
+for v in root.search_2("**abc**"):
+    print (v)
 
-if __name__ == "__main__":
-    root = TrieNode('*')
-    add(root, "hackathon")
-    add(root, 'hack')
 
-    print(find_prefix(root, 'hac'))
-    print(find_prefix(root, 'hack'))
-    print(find_prefix(root, 'hackathon'))
-    print(find_prefix(root, 'ha'))
-    print(find_prefix(root, 'hammer'))
-    print(find_prefix(root, 'ck'))
+
+
+
+
