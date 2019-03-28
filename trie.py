4144345c1d412a222889a025c3d07e76e19a76fe
Tom <zoujinyong@putao.com>
2019-03-28 11:04:55 +0800

    update

--- trie.py
+++ trie.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
 from collections.abc import Set
+import re
 
 class TrieNode(Set):
     def __init__(self, iterable=()):
@@ -31,44 +32,43 @@ class TrieNode(Set):
         return node._end
     
     def search_1(self, term):
-        print ("Start: ", ''.join(term))
         results = set()
         element = []
-        def _search(m, node, i, current_skip_steps, max_skip_steps):
+        
+        def _search(m, node, i, current_skip_steps, min_skip_steps, max_skip_steps):
             element.append(m)
-            print(current_skip_steps, max_skip_steps, ''.join(element), i, len(term))
-            if current_skip_steps <= max_skip_steps and max_skip_steps != -1:
-                #if node._end:
-                #    results.add(''.join(element))
+            #print(current_skip_steps, max_skip_steps, ''.join(element), i, len(term))
+            if current_skip_steps < max_skip_steps and max_skip_steps != -1:
                 for k, child in node._children.items():
-                    _search(k, child, i, current_skip_steps+1, max_skip_steps)
-                    if i+current_skip_steps <= len(term):
-                        _search(k, child, i+current_skip_steps, -1, -1)
-            elif max_skip_steps != -1:
-                pass
-            else:
+                    _search(k, child, i, current_skip_steps+1, min_skip_steps, max_skip_steps)
+                    if current_skip_steps >= min_skip_steps:
+                        _search(k, child, i+1, -1, -1, -1)
+            elif current_skip_steps == -1 and max_skip_steps == -1:
                 if i == len(term):
                     if node._end:
                         results.add(''.join(element))
                 elif term[i] == '?':
                     for k,child in node._children.items():
-                        _search(k, child, i+1, -1, -1)
+                        _search(k, child, i+1, -1, -1, -1)
                 elif term[i] == '*':
-                    _search('', node, i+1, -1, -1)
+                    _search('', node, i+1, -1, -1, -1)
                     for k,child in node._children.items():
-                        _search(k, child, i, -1, -1)
-                elif term[i] == '[W:0-2]':
+                        _search(k, child, i, -1, -1, -1)
+                elif len(re.findall('\[W:0-([1-9]*)\]', term[i])) > 0:
+                    max_s = re.findall('\[W:0-([1-9]*)\]', term[i])[0]
+                    _search('', node, i+1, -1, -1, -1)
                     for k,child in node._children.items():
-                        _search(k, child, i, 1, 2)
-                        _search(k, child, i+1, -1, -1)
+                        _search(k, child, i, 1, 0, int(max_s))
                 elif term[i] in node._children:
-                    _search(term[i], node._children[term[i]], i+1, -1, -1)
-                    
+                    _search(term[i], node._children[term[i]], i+1, -1, -1, -1)
+                elif node._end:
+                    if element[-1] == term[i]:
+                        results.add(''.join(element))
             element.pop()
-        _search('', self, 0, -1, -1)
+        _search('', self, 0, -1, -1, -1)
 
         return results
-    
+    """    
     def search_2(self, term):
         def _add(indexes, i):
             while i < len(term) and term[i] == '*':
@@ -105,6 +105,7 @@ class TrieNode(Set):
                 element.pop()
                 indexes_stack.pop()
                 iter_stack.pop()
+    """
 
     def __iter__(self):
         element = ['']
@@ -123,36 +124,46 @@ class TrieNode(Set):
     def __len__(self):
         return self._len
 
+def print_tree(node, indent=0):
+    if node:
+        ind = '' + '\t' * indent
+        for k,children in node._children.items():
+            label = "'%s' : " % k
+            print (ind + label + '{')
+            print_tree(children, indent+1)
+            print (ind + ' '*len(label) + '}')  
 
-root = TrieNode()
-root.add("aba*")
-root.add("abb")
-root.add("abc")
-root.add("abd")
-root.add("")
-root.add("中华人民")
+def test():
+    root = TrieNode()
+    root.add("aba*")
+    root.add("abb")
+    root.add("abc")
+    root.add("abd")
+    root.add("")
+    root.add("中华人民")
 
 
-root.add("我鱼")
-root.add("我爱鱼")
-root.add("我爱吃鱼")
-root.add("我爱吃大鱼")
-root.add("我爱吃大小鱼")
-root.add("我爱吃大小黄鱼")
-root.add("我狗")
-root.add("我爱狗")
-root.add("我爱吃狗")
-root.add("我爱吃大狗")
-root.add("我爱吃大小狗")
-root.add("我爱吃大小黄狗")
-#print (root.search_1(['我','[W:0-2]','鱼']))
-#print (root.search_1(['[W:0-2]','大','鱼']))
-def print_all(node):
-    ks = []
-    for k,d in node._children.items():
-        ks.append(k)
-    print(' '.join(ks))
-    for k,d in node._children.items():
-        print_all (d)
-print_all(root)
+    root.add("鱼")
+    root.add("我鱼")
+    root.add("我爱鱼")
+    root.add("我爱吃鱼")
+    root.add("我爱吃大鱼")
+    root.add("我爱吃大小鱼")
+    root.add("我爱吃大小黄鱼")
+    root.add("我狗")
+    root.add("我爱狗")
+    root.add("我爱吃狗")
+    root.add("我爱吃大狗")
+    root.add("我爱吃大小狗")
+    root.add("我爱吃大小黄狗")
+    print (root.search_1(['我','[W:0-3]','狗']))
+    print (root.search_1(['[W:0-3]','大','鱼']))
+    print (root.search_1(['我','爱','吃','[W:0-2]', '鱼']))
+    print (root.search_1(['[W:0-2]', '鱼']))
+    print (root.search_1(['我','爱','吃','[W:0-2]', '狗']))
+    print (root.search_1(['[W:0-2]', '狗']))
+    
+    #print_tree(root)
 
+if __name__ == "__main__":
+    test()
